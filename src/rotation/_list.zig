8385a2f1da3508594f147c3109285d7b4c4c11ca
Jeksta <rene.spinger@gmail.com>
2025-03-31 20:36:50 +0200

    Add ladder rotation

--- src/rotation/_list.zig
+++ src/rotation/_list.zig
@@ -1,5 +1,6 @@
 pub const branch = @import("branch.zig");
 pub const carpet = @import("carpet.zig");
+pub const ladder = @import("ladder.zig");
 pub const fence = @import("fence.zig");
 pub const log = @import("log.zig");
 pub const no_rotation = @import("no_rotation.zig");
